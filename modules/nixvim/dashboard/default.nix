30bc2989bfb81b5df864ba610447e1e4e48eb67f
William Diaz <wdiazux@gmail.com>
2023-11-17 00:28:47 -0600

    using lastest update

--- modules/nixvim/dashboard/default.nix
+++ modules/nixvim/dashboard/default.nix
@@ -8,12 +8,12 @@ with lib.plusultra.theme.nord; {
   extraPlugins = [
     (pkgs.vimUtils.buildVimPlugin {
       pname = "dbsession.nvim";
-      version = "unstable-2023-05-31";
+      version = "unstable-2023-06-28";
       src = pkgs.fetchFromGitHub {
         owner = "nvimdev";
         repo = "dbsession.nvim";
-        rev = "8a9d15244190e5f191beef03d50f988e5077446c";
-        sha256 = "12yrmnphspb3nyrsgvmp5m270k5hkw1gna904ypl90smskc4wiyx";
+        rev = "63df28409d940f9cac0a925df09d3dc369db9841";
+        sha256 = "0731yc308jhqwpgabdb69mz8wi75rbnlwa2rh25rrigr28hxin1c";
       };
     })
   ];
@@ -61,7 +61,7 @@ with lib.plusultra.theme.nord; {
       changeToVcsRoot = true;
 
       packages.enable = false;
-      week_header.enable = false;
+      week_header.enable = true;
 
       hide = {
         statusline = true;
